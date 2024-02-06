da8384a20f10d31ca1d5a4392be391ced0324ca0
kylezs <zsembery.kyle@gmail.com>
2024-02-06 15:32:02 +0100

    runner can use shared libraries

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,3 +1,22 @@
+// For the first shared library
+mod libfirst_shared_lib {
+    extern "C" {
+        pub fn entry_point();
+    }
+}
+
+// For the second shared library
+mod libsecond_shared_lib {
+    extern "C" {
+        pub fn entry_point();
+    }
+}
+
 fn main() {
-    print!("Executing runner entry point");
+    println!("Executing runner entry point");
+
+    unsafe {
+        libfirst_shared_lib::entry_point();
+        libsecond_shared_lib::entry_point();
+    }
 }
---
+++ shared2/src/lib.rs
@@ -0,0 +1,4 @@
+#[no_mangle]
+pub extern "C" fn entry_point() {
+    println!("Executing shared2 entry point");
+}
