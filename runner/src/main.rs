207de62bd62af1e63df7e22c0064fc39b1eb0bac
kylezs <zsembery.kyle@gmail.com>
2024-02-08 16:21:27 +0100

    trigger runtime error

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,12 +1,15 @@
 // For the first shared library
 
+// Does a runtime check of the types. If the types are not the same, it will panic.
+// so it panics *before* letting you call it.
 #[stabby::import(name = "first_shared_lib")]
 extern "C" {
-    pub fn entry_point();
+    pub fn entry_point() -> u32;
 }
 
 fn main() {
     println!("Executing runner entry point");
 
-    entry_point();
+    let x = entry_point();
+    println!("Result from shared1: {}", x);
 }
